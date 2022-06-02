95d94cff978b0dec405d11b366efaff98b421f72
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-02 14:50:26 -0700

    calc. function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 from calculator_1 import add, sub, mul, div
 from sys import argv
 
@@ -8,21 +8,19 @@ def main():
     if number_of_arg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    else:
-        if argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            a = argv[1]
-            b = argv[2]
-            if argv[2] == '+':
-                print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
-            elif argv[2] == '-':
-                print("{} + {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
-            elif argv[2] == '*':
-                print("{} + {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
-            elif argv[2] == '/':
-                print("{} + {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+    if argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = argv[1]
+    b = argv[2]
+    if argv[2] == '+':
+        print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
 
 
 if __name__ == "__main__":
