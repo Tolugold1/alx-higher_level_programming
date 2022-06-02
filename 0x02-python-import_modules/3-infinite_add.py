8ac7d75d10f0cc1104429a59c983a17261d22304
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-02 07:21:54 -0700

    sum of argument function

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,16 +3,10 @@ from sys import argv
 
 
 def main():
-    number_of_arg = len(argv) - 1
     i = 0
-    if number_of_arg < 1:
-        print("{}".format(i))
-    elif number_of_arg == 1:
-        print("{}".format(i + int(argv[1])))
-    elif number_of_arg > 1:
-        for j in range(1, len(argv)):
-            i += int(argv[j])
-        print('{}'.format(i))
+    for j in range(1, len(argv)):
+        i += int(argv[j])
+    print('{}'.format(i))
 
 
 if __name__ == "__main__":
