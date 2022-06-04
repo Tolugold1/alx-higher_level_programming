3942b15602f8adefa827d2446df57ee857598f87
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-04 04:00:55 -0700

    max function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_int = 0
-    for i in range(len(my_list)):
-        if max_int < my_list[i]:
-            max_int = my_list[i]
-    return max_int
+    if not my_list:
+        return None
+    else:
+        for i in range(len(my_list)):
+            if max_int < my_list[i]:
+                max_int = my_list[i]
+        return max_int
