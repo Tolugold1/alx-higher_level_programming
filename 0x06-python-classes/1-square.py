2fa010778bbcad97bdfe83e169e3ebc6c7ce26b5
Tolugold1 <toluadelekesteve@gmail.com>
2022-06-21 14:59:55 +0000

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size):
+        """Private instance attribute: size"""
+        self.__size = size
