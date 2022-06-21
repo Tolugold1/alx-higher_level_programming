a5ecb87617c4b851d551397d1eb8e9c1d7387de0
Tolugold1 <toluadelekesteve@gmail.com>
2022-06-21 17:01:57 +0000

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """class Square that defines a square by: (based on 3-square.py)"""
     def __init__(self, size=0):
         """initialize size"""
         self.__size = size
