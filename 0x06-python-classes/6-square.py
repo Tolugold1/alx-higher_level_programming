4b67d59e11dc6d1eb0d4f2da2349af53016fa36d
Tolugold1 <toluadelekesteve@gmail.com>
2022-06-22 07:35:54 +0000

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,7 +16,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """check for size requirments"""
+        """check for size requirements"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -31,7 +31,7 @@ class Square:
     @position.setter
     def position(self, value):
         """test for position requirement"""
-        if value[0] < 0 or value[1] < 0 or type(value) != tuple                or type(value[0]) != int or type(value[1]) != int                or len(value) != 2:
+        if value[0] < 0 or value[1] < 0 or type(value) != tuple                 or type(value[0]) != int or type(value[1]) != int or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
