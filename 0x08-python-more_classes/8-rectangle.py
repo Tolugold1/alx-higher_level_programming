4c95ae300ad83824ee151c3a1ecaa72cc7812b29
Tolugold1 <toluadelekesteve@gmail.com>
2022-06-28 07:51:41 +0000

    Update

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -73,13 +73,11 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """return instance of a rectangle whose area is bigger"""
-        r_area1 = rect_1.area()
-        r_area2 = rect_2.area()
         if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if r_area1 >= r_area2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
