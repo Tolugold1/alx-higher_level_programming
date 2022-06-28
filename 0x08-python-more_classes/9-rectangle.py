a6c352a736aa03a4f4ee06f8386b2e1e4c6e6e24
Tolugold1 <toluadelekesteve@gmail.com>
2022-06-28 08:08:47 +0000

    More classes and objects

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,4 +85,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Return a new instance of rectangle"""
-        return cls(size, size)
+        return Rectangle(size, size)
