de100691dfa3bd4e33eb2a65fe052ac90ce16116
Tolugold1 <toluadelekesteve@gmail.com>
2022-07-04 14:08:59 +0000

    Py-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns the list of available attribute and methods"""
+
+
+def lookup(obj):
+    """return list of attribute and objects"""
+    return dir(obj)
