e432bde68c2294b00501935cb1feb8f5284e26cf
Tolugold1 <toluadelekesteve@gmail.com>
2022-07-05 14:20:56 +0000

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""""function that creates an Object from JSON filea 
+import json
+
+
+def load_from_json_file(filename):
+    """""Creates an Object from JSON filea 
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
