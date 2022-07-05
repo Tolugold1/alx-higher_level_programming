b554e4f4aedfc669cd29478b184ee4e98ac34904
Tolugold1 <toluadelekesteve@gmail.com>
2022-07-05 13:40:47 +0000

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,9 +2,9 @@
 """
 function that returns an object (Python data structure) represented by a JSON string
 """
-import json
 
 
 def from_json_string(my_str):
     """returns an object"""
+    import json
     return json.loads(my_str)
