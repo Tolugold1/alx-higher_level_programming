9e0da9afeb250ce9983efdb6b8dc8a7319005bae
Tolugold1 <toluadelekesteve@gmail.com>
2022-07-05 13:22:59 +0000

    function that append text to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """function that append a text file(UTF8)"""
     with open(filename, 'a') as f:
-        no_of_char = f.write("")
+        no_of_char = f.write(text)
     return no_of_char
