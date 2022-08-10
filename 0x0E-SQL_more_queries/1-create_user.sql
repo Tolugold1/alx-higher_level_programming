27e42322b06c79261f56c68c2df2da9ea89380f8
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-08-10 06:48:14 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Create a user account and grant all permission.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL ON *.* TO user_0d_1;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+GRANT ALL FROM *.* TO user_0d_1;
+FLUSH PRIVILEGES;
