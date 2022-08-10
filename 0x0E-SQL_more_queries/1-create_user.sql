cce0203449f82668a875bba776f7396c90b8a82d
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-08-10 06:51:04 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a user account and grant all permission.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
 SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
-GRANT ALL FROM *.* TO user_0d_1;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++
@@ -1,4 +0,0 @@
--- Create a database and grant select persion only to it's table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXIXTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANTS SELECT ON hbtn_0d_2.* TO user_0d_2;
