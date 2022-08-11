a33acb3a04589fb04d8b7726da3cae2eedb40fa5
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-08-11 03:29:28 -0700

    update

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- Show lists of tv_shows with no genre
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
