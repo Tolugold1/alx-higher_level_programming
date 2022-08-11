8065751f309e5ea6e2abab29d9aa0c4d5ce1d79c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-08-11 04:08:02 -0700

    update

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- lists all genres from hbtn_0d_tvshows
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY COUNT(tv_show_genres.genre_id) DESC;
