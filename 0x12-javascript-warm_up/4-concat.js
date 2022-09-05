d028b8978f9e99f2db1dbf8b03d4f87814c7f30f
Tolulope1 <toluadelekesteve@gmail.com>
2022-09-05 14:12:20 +0000

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-if (process.argv[3] && process.argv[2]) {
-   let arr = [process.argv[2], process.argv[3]];
-   console.log(arr.join(" is "));
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
