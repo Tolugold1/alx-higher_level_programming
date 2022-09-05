0b9e47934c8c5e2e071b68314c395f68e58ad726
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-05 16:19:13 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const x = Number(process.argv[2]);
 if (isNaN(x)) {
    console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x.length; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
