e20837b95e0b6bd7d108547d5ab33c0d3de974ae
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-05 17:23:57 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (n) {
   if (!n || n < 2) {
     return 1;
    }
-   return console.log(n * factorial(n - 1));
+   return n * factorial(n - 1);
 }
 
-factorial(Number(process.argv[2]));
+console.log(factorial(Number(process.argv[2])));
