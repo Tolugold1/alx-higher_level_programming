74fc2e854026b2a8bdda0fed4ac4c73b3a53173b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-06 13:43:59 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,14 @@
 const square = require("./5-square");
 
 class Square extends square {
-   charPrint (c) {
-      if (!c) {
-         c = 'X';
-      } else {
-         for (let i = 0; i < this.width; i++) {
-            console.log(c.repeat(this.width));
-         }
-      }
-   }
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
