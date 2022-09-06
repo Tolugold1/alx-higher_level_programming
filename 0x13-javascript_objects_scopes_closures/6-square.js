b61cb2ac41f5ad0ed087499287ed389099330a58
Tolulope1 <toluadelekesteve@gmail.com>
2022-09-06 12:38:28 +0000

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const square = require("./5-square");
+
+class Square extends square {
+   charPrint (c) {
+      if (!c) {
+         c = 'X';
+      } else {
+         for (let i = 0; i < this.size; i++) {
+            console.log('c'.repeat(this.size));
+         }
+      }
+   }
+}
+
+module.exports = Square;
