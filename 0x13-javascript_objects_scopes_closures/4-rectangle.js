da38ddf567ac110e2cb26c6319f8fd5af95a1359
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-06 13:15:37 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,22 +5,22 @@ class Rectangle {
       this.width = w;
       this.height = h;
     }
-  }
+  };
 
   double () {
     this.width *= 2;
     this.height *= 2;
-  }
+  };
 
   rotate () {
     [this.width, this.height] = [this.height, this.width];
-  }
+  };
 
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-}
+};
  
  module.exports = Rectangle;
