7be4bed0c7a1ae7e9f5a182dba836d530a00ec05
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-09 10:43:32 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities RIGHT JOIN states"
                 " ON cities.state_id = states.id WHERE "
-                "states.name = '{}'").format(sys.argv[4])
+                "states.name = '{}'".format(sys.argv[4]))
     result = cur.fetchall()
-    print(", ".join(row for row in result)
+    print(", ".join(row for row in result))
     cur.close()
     db.close()
