b308947a438f126f1b69bd644a84b5d65ef76c77
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-10 10:21:21 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,6 +4,7 @@ class definition of a State and an instance
  Base = declarative_base()
 """
 
+
 if __name__ == '__main__':
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
