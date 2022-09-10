c5de7945e008e47fbfd5cf9f2ec5e6c31d812a3d
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-10 10:38:06 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,6 +4,8 @@ Write a python file that contains the class definition of a State and an
 instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
