3bfb92733e67e516b73234949b77d0b7fa2df661
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-23 05:26:00 -0700

    update

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL and an email, sends a POST
+"""
+import sys
+
+if __name__ == '__main__':
+    import urllib.request
+    from sys import argv
+    import urllib.parse
+    url = argv[1]
+    value = {'email': argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        email_sent = res.read()
+    print('Your email is: {}'.format(email_sent.decode('utf-8')))
