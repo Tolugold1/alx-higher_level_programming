3569c86f99f82e085443dd113101bcbe2a20d0cb
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-23 14:32:57 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    if len(argv) > 1:
-        q = argv[2]
-    else:
+    if len(argv) == 1:
         q = ""
+    else:
+        q = argv[2]
     req = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
         data = req.json()
