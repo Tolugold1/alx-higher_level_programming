5679647572f93422d97e426e7801d2a776d230d2
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-18 11:25:59 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,9 @@ const request = require('request')
 
 request
 .get(process.argv[2])
-.on('response', (response) => {
+.on('response', 'err', (err, response) => {
+    if (err) {
+        throw err;
+    }
     process.stdout.write('code: ' + response.statusCode)
 })
