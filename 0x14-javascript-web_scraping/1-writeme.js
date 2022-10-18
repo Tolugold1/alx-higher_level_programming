075e1fa2802e5dacc6993e415cbb7f2c36c9a9b7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-18 11:10:24 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 
 if (process.argv[2] && process.argv[3]) {
-    fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+    fs.writeFile(process.argv[2], process.argv[3] + '\n, 'utf-8', (err, data) => {
         if (err) {
         return err
         }
