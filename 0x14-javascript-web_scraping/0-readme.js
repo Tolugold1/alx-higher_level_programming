fafb37abb4530369a81c0f44ee5976bba99456f0
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-18 12:54:47 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
     if (err) {
-        console.log(err);
+        return console.log(err);
     } else {
         process.stdout.write(data);
     }
