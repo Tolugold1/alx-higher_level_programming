1765b26953eb9d2186773ced43633ebe1e866d16
Tolulope <toluadelekesteve@gmail.com>
2022-10-18 23:16:09 -0700

    update

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, (error, response, body) => {
+    if (error) {
+        throw error;
+    } else {
+        fs.wirteFile(filePath, body, "utf-8", (err, data) => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
+});
